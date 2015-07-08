866e27a069b5d368d4ebdc2b3d3c2d7f488fed2e
rahul sahay <rahulsahay19@gmail.com>
2015-07-08 10:23:57 +0530

    Modified

--- app/js/controllers/MovieController.js
+++ app/js/controllers/MovieController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 moviesApp.controller('MovieController',function MovieController($scope){
-	$scope.event ={
+	$scope.show ={
 		name: 'Jurassic World',
 		date: '12/06/2015',
 		time: '07:30 pm'
